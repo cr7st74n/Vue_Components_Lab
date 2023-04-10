7d0ce2edba69e34fe03cd261edaadb227874429c
cr7st74n <cris-082011@hotmail.com>
2023-04-10 18:53:44 -0400

    adding 1st program

--- wouldyourather/src/App.vue
+++ wouldyourather/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="App">
 
-    <h1>Would You Rather ??</h1>
-    <p>hello</p>
-
-    <WouldYouRather msg="hola"/>
+    <h1>Would You Rather...</h1>
+    <WouldYouRather v-bind:question = "wyrQuestion"
+    v-bind:answer1 = "wyrAnswer1"
+    v-bind:answer2 = "wyrAnswer2"
+    v-on:answer-changed="answerChanged"
+    >
+    </WouldYouRather>
+    <p>{{ UserSelectionMessage  }}</p>
   </div>
 </template>
 
@@ -15,6 +19,19 @@ export default {
   name: 'App',
   components: {
     WouldYouRather
+  },
+  data () {
+    return {
+      wyrQuestion: 'Would you rather have your very own house next to your parents house or live with your parents in a house that is twice the size of the one you live in now?',
+      wyrAnswer1: 'Own hause next to my parents',
+      wyrAnswer2: 'House twice the size of the one you live in now',
+      UserSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged (choice) {
+      this.UserSelectionMessage = `Thanks!  you chose ${choice}`
+    }
   }
 }
 </script>
--- wouldyourather/src/components/WouldYouRather.vue
+++ wouldyourather/src/components/WouldYouRather.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h2> Please make your choice!</h2>
+
+    <h3>{{ question }}</h3> <br>
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade"><label>{{ answer1 }}</label><br>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade"><label>{{ answer2 }}</label>
   </div>
 </template>
 
@@ -8,7 +12,19 @@
 export default {
   name: 'WouldYouRather',
   props: {
-    msg: String
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  data () {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade () {
+      this.$emit('answer-changed', this.choice)
+    }
   }
 }
 </script>
